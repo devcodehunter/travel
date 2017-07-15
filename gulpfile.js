1bdcd5ce049f20985f6448e19c38258e5c5f1305
G M Morshed <shawonahmed78@gmail.com>
2017-07-15 10:59:38 +0600

    browserSync Worked Properly

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ gulp_postcss_simple_vars=require('postcss-simple-vars'),
 gulp_postcss_nested=require('postcss-nested'),
 gulp_postcss_import=require('postcss-import'),
 mixins=require('postcss-mixins');
+browserSync=require('browser-sync').create();
 
 
 gulp.task("default", function(){
@@ -13,21 +14,40 @@ gulp.task("default", function(){
 });
 
 gulp.task('html', function(){
-	console.log("Html Found.");
+	browserSync.reload();
 });
 
 gulp.task('styles', function(){
 	gulp.src('./app/assets/styles/styles.css')
 	.pipe(gulp_postcss([gulp_postcss_import, mixins, gulp_autoprefixer, gulp_postcss_simple_vars, gulp_postcss_nested]))
+	.on('error', function(errorInfo){
+		console.log(errorInfo.toString());
+		this.emit('end');
+	})
 	.pipe(gulp.dest('./app/temp/styles'));
 });
 
+
+
 gulp.task('watch', function(){
+
+	browserSync.init({
+		notify:false,
+		server:{
+			baseDir: "app"
+		}
+	});
+
 	gulp_watch('./app/index.html', function(){
 		gulp.start('html');
 	});
 
 	gulp_watch('./app/assets/styles/**/*.css', function(){
-		gulp.start('styles');
+		gulp.start('cssInject');
 	});
+});
+
+gulp.task('cssInject',['styles'], function(){
+	gulp.src('./app/temp/styles/styles.css')
+	.pipe(browserSync.stream());
 });
